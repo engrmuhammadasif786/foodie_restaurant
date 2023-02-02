ff9230ab66712c08dccf513fb5e6ec83fb8e2a03
engrmuhammadasif786 <84713360+engrmuhammadasif786@users.noreply.github.com>
2023-02-02 18:55:31 +0500

    first commit

---
+++ app/Http/Controllers/PayoutsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\VendorUsers;
+
+class PayoutsController extends Controller
+{  
+
+   public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+    	$user = Auth::user();
+        $id = Auth::id();
+        $exist = VendorUsers::where('user_id',$id)->first();
+        $id=$exist->uuid;
+       	return view("restaurants_payouts.index")->with('id',$id);
+    }
+
+}
